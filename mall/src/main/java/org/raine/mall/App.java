47ceeed7651759c43eca3540fc549d93f2cca858
yidayo <418680861@qq.com>
2020-08-06 15:37:19 +0800

    2020/8/6

---
+++ mall/src/main/java/org/raine/mall/App.java
@@ -0,0 +1,13 @@
+package org.raine.mall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
