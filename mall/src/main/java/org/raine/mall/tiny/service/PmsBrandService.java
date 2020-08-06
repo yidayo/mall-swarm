4f2160ae84261150abe7faf0c9c64963aeba187f
yidayo <418680861@qq.com>
2020-08-06 21:46:45 +0800

    2020/8/6

---
+++ mall/src/main/java/org/raine/mall/tiny/service/PmsBrandService.java
@@ -0,0 +1,22 @@
+package org.raine.mall.tiny.service;
+
+import java.util.List;
+
+import org.raine.mall.tiny.mbg.model.PmsBrand;
+
+/**
+ * PmsBrandService
+ */
+public interface PmsBrandService {
+    List<PmsBrand> listAllBrand();
+
+    int createBrand(PmsBrand brand);
+
+    int updateBrand(Long id, PmsBrand brand);
+
+    int deleteBrand(Long id);
+
+    List<PmsBrand> listBrand(int pageNum, int pageSize);
+
+    PmsBrand getBrand(Long id);
+}
